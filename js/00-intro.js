06019987b98d87128bab77bb1a56d120382698fc
Paulius B <Paulspremium@gmail.com>
2021-05-04 18:55:55 +0300

    jsintro

---
+++ js/00-intro.js
@@ -0,0 +1,21 @@
+//single-line comment
+
+/*
+multi
+line
+comment
+*/
+
+/*
+Konceptai:
+
+1) kintamieji - isimina informacija;
+2) salygos sakiniai - tarpusavio palyginimas;
+3) ciklai - darbas su pasikartojancio formato informacija,
+pasikartpjanciu procesu valdymas;
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal 
+poreiki;
+5) ivykiai - uzsikrove puslapis, paspaude klaviaturos mygtuka,
+issiunte uzklause... 
+6) oop- object oriented programming(objektinis programavimas)
+*/

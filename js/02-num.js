5fb6cc1ad4ecec1b63a083ad899de835a9caa8ca
Paulius B <Paulspremium@gmail.com>
2021-05-04 20:08:16 +0300

    numbers

---
+++ js/02-num.js
@@ -0,0 +1,30 @@
+/*
+NUMBERS:
+
+teigiami, nulis, neigiami - matematikoje
+
+teigiami, neigiami - programavime
+
+desimtainiai, sveikieji
+tikri skaiciai, begalybes, NaN
+*/
+
+const teigiamas = 5;
+const neigiamas = -8;
+const nulis = 0;
+const nulisMinus = -0;
+
+console.log(nulis, nulisMinus);
+
+const desimtainis = 3.14159258;
+console.log(desimtainis);
+
+const begalybe = Infinity;
+const begalybeMinus = -Infinity;
+console.log(begalybe, begalybeMinus);
+
+const nesamone = NaN;
+console.log(nesamone);
+
+const puseZodzio = 'labas'/ 2;
+console.log(puseZodzio);

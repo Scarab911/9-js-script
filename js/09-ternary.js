8ccdb7313f7ab52f4723f2c43522a0245bbd1a90
Paulius B <Paulspremium@gmail.com>
2021-05-06 18:59:02 +0300

    nestintas ternaris

--- js/09-ternary.js
+++ js/09-ternary.js
@@ -11,3 +11,13 @@ console.log(sketis);
 
 const skaicius = 4 > 2 ? `daugiau` : `ne daugiau`;
 console.log(skaicius);
+
+console.log(`------------------------`);
+
+// nestintas ternary
+
+const color = `blue`;
+const light = true;
+
+const x1 = color === `blue` ? light ? `Sviesiai melyna` : `Tamsiai Melyna` : color === `red` ? `Raudona` : `nezinoma spalva`;
+console.log(x1);

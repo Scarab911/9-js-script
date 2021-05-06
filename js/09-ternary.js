83838b3cf414b457ee0c3412f566531e9c3b557b
Paulius B <Paulspremium@gmail.com>
2021-05-06 18:49:13 +0300

    ternary

---
+++ js/09-ternary.js
@@ -0,0 +1,13 @@
+/*
+TERNARY OPERATOR
+klausimas ? teigiama : neigiama
+
+*/
+
+const arLyja = true;
+
+const sketis = arLyja ? `pasiimk` : `nereikia`;                    //`pasiimk` / `nereikia`
+console.log(sketis);
+
+const skaicius = 4 > 2 ? `daugiau` : `ne daugiau`;
+console.log(skaicius);

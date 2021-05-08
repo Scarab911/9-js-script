964e9b8ff9770b85d2cc933fd38e59313f478d9e
Paulius B <Paulspremium@gmail.com>
2021-05-08 13:55:01 +0300

    allstar

---
+++ js/codewars/allstar.js
@@ -0,0 +1,22 @@
+function strCount(str, letter){ 
+    let kiekis = 0;
+
+        for (let i = 0; i < str.length; i++) {
+
+            const raide = str[i];
+                if (raide === letter) {
+
+                    console.log(raide);
+                    kiekis += + 1;
+
+                }
+        }
+return kiekis;
+}
+
+
+
+
+console.log(strCount('Hello', 'o'), `->`, 1);
+console.log(strCount('Hello', 'l'), `->`, 2);
+console.log(strCount('', 'z'), `->`, 0);
